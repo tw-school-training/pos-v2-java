98038adb9c29800361ec4617477a83046213939b
Qiana <xueqian0321@outlook.com>
2019-10-11 15:56:30 +0800

    init repo

---
+++ src/main/java/com/thoughtworks/pos/Printer.java
@@ -0,0 +1,12 @@
+package com.thoughtworks.pos;
+
+
+public class Printer {
+    public String print(String[] cart, String[] promotions) {
+
+        // Implementation
+
+        return "";
+    }
+}
+
